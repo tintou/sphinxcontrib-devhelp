97d279cd080ac3af1d78b6d47f6a3c3bb89e74f7
Corentin Noël <corentin.noel@collabora.com>
2026-06-18 09:24:29 +0200

    Make the extension output devhelp2 files
    The devhelp2 files have been replacing the devhelp files since early 2000 it is
    safe to assume that every devhelp reader can now read them.

--- sphinxcontrib/devhelp/__init__.py
+++ sphinxcontrib/devhelp/__init__.py
@@ -25,6 +25,8 @@ if TYPE_CHECKING:
 
 import xml.etree.ElementTree as etree
 
+_DEVHELP2_NS = 'http://www.devhelp.net/book'
+
 __version__ = '2.0.0'
 __version_info__ = (2, 0, 0)
 
@@ -65,11 +67,15 @@ class DevhelpBuilder(StandaloneHTMLBuilder):
         logger.info(__('dumping devhelp index...'))
 
         # Basic info
-        root = etree.Element('book',
-                             title=self.config.html_title,
-                             name=self.config.project,
-                             link="index.html",
-                             version=self.config.version)
+        root = etree.Element('book')
+        root.set("xmlns", "http://www.devhelp.net/book")
+        root.set("title", self.config.html_title)
+        root.set("link", "index.html")
+        root.set("author", self.config.author)
+        root.set("name", self.config.project)
+        root.set("version", "2")
+        if self.config.html_baseurl:
+            root.set("online", self.config.html_baseurl)
         tree = etree.ElementTree(root)
 
         # TOC
@@ -102,12 +108,12 @@ class DevhelpBuilder(StandaloneHTMLBuilder):
             if len(refs) == 0:
                 pass
             elif len(refs) == 1:
-                etree.SubElement(functions, 'function',
-                                 name=title, link=refs[0][1])
+                etree.SubElement(functions, 'keyword',
+                                 type='function', name=title, link=refs[0][1])
             else:
                 for i, ref in enumerate(refs):
-                    etree.SubElement(functions, 'function',
-                                     name="[%d] %s" % (i, title),
+                    etree.SubElement(functions, 'keyword',
+                                     type='function', name="[%d] %s" % (i, title),
                                      link=ref[1])
 
             if subitems:
@@ -121,9 +127,8 @@ class DevhelpBuilder(StandaloneHTMLBuilder):
                 write_index(title, refs, subitems)
 
         # Dump the XML file
-        xmlfile = path.join(outdir, outname + '.devhelp.gz')
-        with gzip.GzipFile(filename=xmlfile, mode='w', mtime=0) as f:
-            tree.write(f, 'utf-8')
+        xmlfile = path.join(outdir, outname + '.devhelp2')
+        tree.write(xmlfile, encoding="UTF-8")
 
 
 def setup(app: Sphinx) -> dict[str, Any]:
